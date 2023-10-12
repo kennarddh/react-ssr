b07fe2abe425e0b119f82cdee4793c2affd3cdf6
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-10-12 20:12:24 +0700

    feat: Cache

--- src/Server/production.ts
+++ src/Server/production.ts
@@ -7,8 +7,49 @@ import { fileURLToPath } from 'url'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-async function createServer() {
+const GenerateHTML = async () => {
+	// Read index.html
+	const template = fs.readFileSync(
+		path.resolve(__dirname, '../../build/client/index.html'),
+		'utf-8',
+	)
+
+	// EntryServ = EntryServer
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	// eslint-disable-next-line import/no-unresolved
+	const EntryServ = await import('../../build/server/EntryServer.js')
+
+	const EntryJSX = EntryServ.EntryJSX as () => unknown
+
+	// Build styled-componets css
+	const sheet = new ServerStyleSheet()
+	const styledAppliedJSX = sheet.collectStyles(EntryJSX())
+
+	// Refactor https://styled-components.com/docs/advanced#streaming-rendering to use response streaming
+	/**
+	 * Don't refactor to get all styles and then render because it won't work
+	 * https://styled-components.com/docs/advanced#example See this note
+	 * sheet.getStyleTags() and sheet.getStyleElement() can only be called after your element is rendered.
+	 */
+	const appHtml = ReactDOMServer.renderToString(styledAppliedJSX)
+	const styleTags = sheet.getStyleTags()
+	sheet.seal()
+
+	// Inject the app-rendered HTML into the template.
+	let html = template.replace('<!--ssr-outlet-->', appHtml)
+
+	// Inject styles into the template
+	html = html.replace('<!--styles-outlet-->', styleTags)
+
+	return html
+}
+
+const CreateServer = async () => {
 	const app = express()
+
+	const html = await GenerateHTML()
+
 	app.use(
 		express.static(path.resolve(__dirname, '../../build/client/'), {
 			index: false,
@@ -17,41 +58,6 @@ async function createServer() {
 
 	app.use('*', async (req: Request, res: Response, next: NextFunction) => {
 		try {
-			// Read index.html
-			const template = fs.readFileSync(
-				path.resolve(__dirname, '../../build/client/index.html'),
-				'utf-8',
-			)
-
-			// EntryServ = EntryServer
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-ignore
-			// eslint-disable-next-line import/no-unresolved
-			const EntryServ = await import('../../build/server/EntryServer.js')
-
-			const EntryJSX = EntryServ.EntryJSX as () => unknown
-
-			// Build styled-componets css
-			const sheet = new ServerStyleSheet()
-			const styledAppliedJSX = sheet.collectStyles(EntryJSX())
-
-			// Refactor https://styled-components.com/docs/advanced#streaming-rendering to use response streaming
-			/**
-			 * Don't refactor to get all styles and then render because it won't work
-			 * https://styled-components.com/docs/advanced#example See this note
-			 * sheet.getStyleTags() and sheet.getStyleElement() can only be called after your element is rendered.
-			 */
-			const appHtml = ReactDOMServer.renderToString(styledAppliedJSX)
-			const styleTags = sheet.getStyleTags()
-			sheet.seal()
-
-			// Inject the app-rendered HTML into the template.
-			let html = template.replace('<!--ssr-outlet-->', appHtml)
-
-			// Inject styles into the template
-			html = html.replace('<!--styles-outlet-->', styleTags)
-
-			//  Send the rendered HTML
 			res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
 		} catch (error) {
 			next(error)
@@ -63,4 +69,4 @@ async function createServer() {
 	})
 }
 
-createServer()
+CreateServer()
